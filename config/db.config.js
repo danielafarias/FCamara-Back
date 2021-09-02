c0ee0bd124d091ce0b5d893bed0ced2b7324ec4a
Luisa <luisa.santos042@gmail.com>
2021-09-02 17:45:31 -0300

    redefine scripts e entry point

--- config/db.config.js
+++ config/db.config.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
 mongoose
-.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
-.then((x) => {
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((x) => {
     console.log(`Connected to Mongo! Database name: ${x.connections[0].name}`);
-})
-.catch((err) => {
+  })
+  .catch((err) => {
     console.error(`Error connecting to Mongo: ${err}`);
-})
+  });
