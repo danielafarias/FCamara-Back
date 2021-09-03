818310a41ad9bc63780bfe3e2e36f98b2e9e2761
Luisa <luisa.santos042@gmail.com>
2021-09-03 14:51:04 -0300

    modifica schema do usuario

--- models/User.js
+++ models/User.js
@@ -1,28 +1,22 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    nome: {
-        type: String,
-        require: true,
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: true,
-        lowercase: true,
+  nome: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
 
-    },
-    password: {
-        type: String,
-        required: true,
-        select: false,
-    },
-    unidade: {
-        type: String,
-        require: true,
-    }
-
-})
-
-const User = mongoose.model("User", UserSchema);
+const User = mongoose.model('User', UserSchema);
 module.exports = User;
