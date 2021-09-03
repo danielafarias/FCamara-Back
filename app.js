5fd352469a2e812fa822ad2ec29d4d3bd662c678
Luisa <luisa.santos042@gmail.com>
2021-09-03 14:39:55 -0300

    atualiza middleware
    remove body-parser, usa express

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
 require('dotenv').config();
 
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 
 // Connect Database
-
 require('./config/db.config');
 
 const app = express();
 
 // Middlewares
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 require('./controllers/authController')(app);
 
 // Rotas
 app.get('/', (req, res) => {
-    res.send('ok');
-})
-// Exportar o app
+  res.send('ok');
+});
 
+// Exportar o app
 module.exports = app;
