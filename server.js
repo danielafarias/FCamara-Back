f46b013219c13669954cec6d9d050ffc896549b2
Luisa <luisa.santos042@gmail.com>
2021-09-03 15:26:46 -0300

    altera 'server.js'

--- server.js
+++ server.js
@@ -1,8 +1,4 @@
-const http = require('http');
-
-const app = require('./app');
-
-const server = http.createServer(app);
+const server = require('./app');
 
 const port = process.env.PORT ? parseInt(process.env.PORT) : 5000;
 const hostname = process.env.HOSTNAME || '127.0.0.1';
