499836cd65c27754219b20a19f6051e0cada5ceb
Luisa <luisa.santos042@gmail.com>
2021-09-03 15:12:16 -0300

    atualiza '.env.example'
    adiciona variaveis

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = require('./app');
 
 const server = http.createServer(app);
 
-server.listen(process.env.PORT, () => {
-    console.log(`Servidor ouvindo na porta ${process.env.PORT}`);
+const port = process.env.PORT ? parseInt(process.env.PORT) : 5000;
+const hostname = process.env.HOSTNAME || '127.0.0.1';
+
+server.listen(port, () => {
+  console.log(`Servidor ouvindo em ${hostname}:${port}`);
 });
